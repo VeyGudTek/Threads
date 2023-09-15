b1417a8d107423ec7b11737b81702e4ef780b8f4
Tim Lin <lintim03@gmail.com>
2023-09-15 11:38:04 -0500

    Removed user page and query functions

--- query_commands.py
+++ query_commands.py
@@ -25,7 +25,6 @@ def get_page(cur, page, user_input, query, from_user):
     else:
         return True, int(user_input)
     
-
 def get_order(order_by, user_input):
     if user_input == "title":
         return True, 'title'
@@ -60,39 +59,3 @@ def get_from_user(cur, cur_from_user, user_input):
     else:
         print(f'No user called "{user_input}"')
         return False, ''
-
-def get_page_users(cur, page, user_input, query):
-    query_text = "%" + query + "%"
-
-    cur.execute('SELECT COUNT(*) FROM users WHERE username ILIKE %s;', (query_text, ))
-    max_pages = math.ceil(cur.fetchone()[0]/10)
-
-    if not user_input and page < max_pages:
-        return True, page + 1
-    elif not user_input:
-        print("You are already on (or past) the last page.")
-        print("To jump to a certain page, provide a page number in the format: page [page number]")
-        print("To jump to the last page, enter an arbitrarily large number for [page number]")
-        return False, max_pages
-    elif not user_input.isdigit():
-        print("Please enter a valid page number.")
-        return False, page
-    elif int(user_input) < 1:
-        print("Please enter a valid page number.")
-        return False, page
-    elif int(user_input) > max_pages:
-        return True, max_pages
-    else:
-        return True, int(user_input)
-
-def get_query_users(cur_query, user_input):
-    if len(user_input) > 30:
-        print("Usernames do not exceed 30 characters.")
-        return False, ''
-    elif not cur_query and not user_input:
-        print('Search already cleared. If you want to perform a search, provide a query in the format: searchusers [query]')
-        return False, user_input
-    else:
-        return True, user_input
-    
-    
